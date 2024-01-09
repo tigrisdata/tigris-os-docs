cab0742e2c082374cffa4939b266b76a7bc50046
Ovais Tariq <ot@tigrisdata.com>
2024-01-09 14:39:36 -0800

    update

--- tigris.config.js
+++ tigris.config.js
@@ -4,6 +4,6 @@ module.exports = {
   earlyAccessUrl: "https://hello.tigrisdata.com/forms/early-access/",
   consoleUrl: "https://console.tigris.dev/",
   blogUrl: "https://www.tigrisdata.com/blog/",
-  websiteUrl: "https://www.tigrisdata.com/",
+  websiteUrl: "https://www.tigrisdata.com",
   discordUrl: "https://www.tigrisdata.com/discord/",
 };

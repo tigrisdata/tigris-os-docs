f88718b1c0c43fd35063a2f1dc4e67d2e757c781
Ovais Tariq <ot@tigrisdata.com>
2024-02-14 11:48:51 -0800

    Minor updates to docs

--- docs/get-started/index.md
+++ docs/get-started/index.md
@@ -19,8 +19,7 @@ keys that you explicitly grant access permissions to.
 
 To use Tigris you need a [Fly.io](https://fly.io/) account. If you don't have
 one, you can get yourself one [here](https://fly.io/app/sign-up). Then
-[signup for the waitlist](https://hello.tigrisdata.com/forms/early-access/) and
-we will reach out to you as soon as possible.
+[signup for eary access](https://hello.tigrisdata.com/forms/early-access/).
 
 ### 2. Create a bucket
 
--- src/theme/Footer/Layout/index.jsx
+++ src/theme/Footer/Layout/index.jsx
@@ -24,7 +24,7 @@ export default function FooterLayout({ style, links, logo, copyright }) {
           <div className="footer__row">
             <div className="footer__data">
               <div className="footer__cta">
-                <Link href={tigrisConfig.earlyAccessUrl}>Early Access</Link>
+                <Link href={tigrisConfig.earlyAccessUrl}>Signup</Link>
               </div>
             </div>
             <div className="links">{links}</div>

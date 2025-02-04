e7059de17593358619c44a3119cdbd51f729ee33
Xe Iaso <xe@tigrisdata.com>
2025-02-04 11:49:45 -0500

    docs/sdks/s3/python: move examples to the filesystem, ensure they work
    Signed-off-by: Xe Iaso <xe@tigrisdata.com>

---
+++ examples/python/object-regions.py
@@ -0,0 +1,17 @@
+import boto3
+
+# Create S3 service client
+svc = boto3.client('s3', endpoint_url='https://fly.storage.tigris.dev')
+
+# Restrict data to Europe (Frankfurt) only
+def _limit_to_fra(request, **kwargs):
+    request.headers.add_header('X-Tigris-Regions', 'fra')
+
+# Register event into boto
+svc.meta.events.register(
+    "before-sign.s3.PutObject",
+    _limit_to_fra,
+)
+
+# Upload file to frankfurt
+response = svc.upload_file('bar.txt', 'tigris-example', 'bar.txt')
---
+++ examples/python/presigned-urls.py
@@ -0,0 +1,22 @@
+import boto3
+
+# Create S3 service client
+svc = boto3.client('s3', endpoint_url='https://fly.storage.tigris.dev')
+
+# Generate a presigned URL to upload an object
+url = svc.generate_presigned_url(
+    'put_object',
+    Params={'Bucket': 'foo-bucket', 'Key': 'bar.txt'},
+    ExpiresIn=604800
+)
+
+print(f'Presigned URL to upload an object: {url}')
+
+# Generate a presigned URL to download an object
+url = svc.generate_presigned_url(
+    'get_object',
+    Params={'Bucket': 'foo-bucket', 'Key': 'bar.txt'},
+    ExpiresIn=604800
+)
+
+print(f'Presigned URL to download an object: {url}')

8dc90d90605070e297d0e5323430ce4e51a58627
Xe Iaso <xe@tigrisdata.com>
2025-02-10 13:23:51 -0500

    docs/sdks/s3/dotnet: move examples to their own files
    Signed-off-by: Xe Iaso <xe@tigrisdata.com>

---
+++ examples/dotnet/GettingStarted.cs
@@ -0,0 +1,54 @@
+﻿using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+IAmazonS3 s3Client = new AmazonS3Client(
+    new AmazonS3Config { ServiceURL = "https://fly.storage.tigris.dev" }
+);
+
+var bucketName = "tigris-example";
+
+// List buckets
+var listResponse = await s3Client.ListBucketsAsync();
+
+Console.WriteLine("Buckets:");
+foreach (var s3Bucket in listResponse.Buckets)
+{
+    Console.WriteLine("* {0}", s3Bucket.BucketName);
+}
+
+// PutObject
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName = bucketName,
+    Key = "test_object_key",
+    ContentBody = "Test object data",
+    UseChunkEncoding = false,
+};
+
+var responsePut = await s3Client.PutObjectAsync(putObjectRequest);
+
+Console.WriteLine($"PUT {responsePut.ETag}");
+
+// GetObject
+var getRequest = new GetObjectRequest { BucketName = bucketName, Key = "test_object_key" };
+
+using (var responseGet = await s3Client.GetObjectAsync(getRequest))
+{
+    using (var reader = new StreamReader(responseGet.ResponseStream))
+    {
+        var content = reader.ReadToEnd();
+        Console.WriteLine($"GET '{content}'");
+    }
+}
+
+// List objects
+var request = new ListObjectsV2Request { BucketName = bucketName };
+
+var response = await s3Client.ListObjectsV2Async(request);
+
+foreach (var s3Object in response.S3Objects)
+{
+    Console.WriteLine("{0}", s3Object.Key);
+}

60f015950fab8a448c60cf5bf80e4e1f809d9fee
Xe Iaso <xe@tigrisdata.com>
2025-02-19 13:09:24 -0500

    docs: add examples for rename support
    Closes TIG-3632

---
+++ examples/go/cmd/rename-objects/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/smithy-go/transport/http"
+)
+
+func WithHeader(key, value string) func(*s3.Options) {
+	return func(options *s3.Options) {
+		options.APIOptions = append(options.APIOptions, http.AddHeaderValue(key, value))
+	}
+}
+
+func WithRename() func(*s3.Options) {
+	return func(options *s3.Options) {
+		options.APIOptions = append(options.APIOptions, http.AddHeaderValue("X-Tigris-Rename", "true"))
+	}
+}
+
+func main() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		log.Fatalf("usage: %s <bucket>", flag.Arg(0))
+	}
+
+	bucketName := flag.Arg(0)
+	keyName := "examplefile-go.js"
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Printf("Couldn't load default configuration. Here's why: %v\n", err)
+		return
+	}
+
+	// Create S3 service client
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String("https://fly.storage.tigris.dev")
+		o.Region = "auto"
+		o.UsePathStyle = false
+	})
+
+	contentType := "text/javascript"
+
+	fmt.Println("Putting object to the bucket:", keyName)
+
+	// put a javascript file in the bucket
+	_, err = client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:      aws.String(bucketName),
+		Key:         aws.String(keyName),
+		Body:        bytes.NewBuffer([]byte("console.log('Hello, World!')")),
+		ContentType: aws.String(contentType),
+	})
+	if err != nil {
+		log.Fatalf("Unable to put object. Here's why: %v", err)
+	}
+
+	targetName := "examplefile-go-rename.js"
+
+	fmt.Println("Renaming object in the bucket:", keyName)
+
+	// rename the object in the bucket
+	_, err = client.CopyObject(ctx, &s3.CopyObjectInput{
+		Bucket:     aws.String(bucketName),
+		CopySource: aws.String(bucketName + "/" + keyName),
+		Key:        aws.String(targetName),
+	}, WithRename())
+	if err != nil {
+		log.Fatalf("Unable to rename object. Here's why: %v", err)
+	}
+
+	fmt.Println("Object renamed successfully to:", targetName)
+
+	fmt.Println("Deleting object from the bucket:", targetName)
+
+	// delete the object from the bucket
+	_, err = client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(targetName),
+	})
+	if err != nil {
+		log.Fatalf("Unable to delete object. Here's why: %v", err)
+	}
+
+	fmt.Println("Object deleted successfully:", targetName)
+}
---
+++ examples/js/rename-object.js
@@ -0,0 +1,65 @@
+import {
+  S3Client,
+  PutObjectCommand,
+  CopyObjectCommand,
+  DeleteObjectCommand,
+  HeadObjectCommand,
+} from "@aws-sdk/client-s3";
+import { randomUUID } from "node:crypto";
+
+export const renameObject = async (S3, bucket, oldKey, newKey) => {
+  S3.middlewareStack.add(
+    (next) => async (args) => {
+      // eslint-disable-next-line no-param-reassign
+      args.request.headers["X-Tigris-Rename"] = "true";
+      return next(args);
+    },
+    {
+      step: "build",
+      name: "renameObject",
+      tags: ["METADATA", "RENAME"],
+    }
+  );
+
+  const copyCommand = new CopyObjectCommand({
+    Bucket: bucket,
+    CopySource: `${bucket}/${oldKey}`,
+    Key: newKey,
+  });
+
+  await S3.send(copyCommand);
+
+  S3.middlewareStack.remove("renameObject");
+};
+
+const S3 = new S3Client({
+  region: "auto",
+  s3ForcePathStyle: false,
+  endpoint: "https://fly.storage.tigris.dev",
+});
+
+const bucket = "tigris-example";
+const object = randomUUID();
+const newObject = randomUUID();
+
+let command = new PutObjectCommand({
+  Bucket: bucket,
+  Key: object,
+  Body: "Hello, Tigris!",
+});
+await S3.send(command);
+
+console.log("Rename object");
+await renameObject(S3, bucket, object, newObject);
+
+command = new HeadObjectCommand({
+  Bucket: bucket,
+  Key: newObject,
+});
+await S3.send(command);
+
+command = new DeleteObjectCommand({
+  Bucket: bucket,
+  Key: newObject,
+});
+await S3.send(command);

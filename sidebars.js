b00377d7b524f4536ff3be638178ecdb370c9ec0
Garren <garren@tigrisdata.com>
2025-04-29 16:57:15 +0200

    fix: add lifecycle transition docs (#186)
    * fix: add lifecycle transition docs
    
    * add loom video
    
    * add in bucket settings and update video

--- sidebars.js
+++ sidebars.js
@@ -63,6 +63,7 @@ const sidebars = {
             "buckets/cors",
             "buckets/custom-domain",
             "buckets/objects-expiration",
+            "buckets/object-lifecycle",
             "buckets/object-notifications",
             "buckets/settings/index",
             "buckets/sharing",

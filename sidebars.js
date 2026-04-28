b2127652cdb91092c0b29d7c975c98657c50cc11
David Myriel <davidmyriel@gmail.com>
2026-04-28 16:48:48 +0200

    add guide

--- sidebars.js
+++ sidebars.js
@@ -462,6 +462,11 @@ const sidebars = {
                 },
                 {
                   type: "doc",
+                  label: "DuckLake",
+                  id: "quickstarts/ducklake",
+                },
+                {
+                  type: "doc",
                   label: "Databricks",
                   id: "libraries/databricks/index",
                 },
